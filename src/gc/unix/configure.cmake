48919927d2971de1ad64fef61141c7e5b9a86070
Sean Gillespie <segilles@microsoft.com>
2017-05-02 16:04:32 -0700

    Get rid of the separate 'GC PAL' build

--- src/gc/unix/configure.cmake
+++ src/gc/unix/configure.cmake
@@ -52,4 +52,4 @@ check_cxx_source_runs("
     }
     " HAVE_MACH_ABSOLUTE_TIME)
 
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/config.h.in ${CMAKE_CURRENT_BINARY_DIR}/config.h)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/unix/config.h.in ${CMAKE_CURRENT_BINARY_DIR}/config.h)
